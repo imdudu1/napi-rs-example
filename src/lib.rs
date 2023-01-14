bd6af55a97925e5384195b0ab46cf5c9ab10cf67
Yohan <cd80@kakao.com>
2023-01-14 23:29:19 +0900

    test(): plus_100, minus_100 테스트 추가

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![deny(clippy::all)]
 
+use std::cmp::min;
+
 use napi_derive::napi;
 
 #[napi]
@@ -10,4 +12,18 @@ pub fn plus_100(input: u32) -> u32 {
 #[napi]
 pub fn minus_100(input: i32) -> i32 {
   input - 100
+}
+
+#[test]
+fn test_plus_100() {
+  let expect = 110;
+
+  assert_eq!(plus_100(10), expect);
+}
+
+#[test]
+fn test_minus_100() {
+  let expect = -90;
+
+  assert_eq!(minus_100(10), expect);
 }
